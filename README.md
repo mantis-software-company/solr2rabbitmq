04656f6473a5185437c2e08137dd411107c75018
Furkan Kalkan <furkankalkan@mantis.com.tr>
2021-10-05 16:17:10 +0300

    Add scalability feature

--- README.md
+++ README.md
@@ -34,7 +34,6 @@ if __name__ == '__main__':
        "mq_pass": os.environ.get('MQ_PASS'),
        "mq_exchange": os.environ.get('MQ_EXCHANGE'),
        "mq_routing_key": os.environ.get("MQ_ROUTING_KEY"),
-       "mq_queue_durable": bool(strtobool(os.environ.get('MQ_QUEUE_DURABLE', 'True'))),
        "solr_collection_url": os.environ.get("SOLR_COLLECTION_URL"),
        "solr_fetch_size": int(os.environ.get("SOLR_FETCH_SIZE")),
        "solr_indexdate_field": os.environ.get("SOLR_INDEXDATE_FIELD"),
@@ -59,8 +58,11 @@ You can also call this library as standalone job command.  Just set required env
 - MQ_VHOST
 - MQ_USER
 - MQ_PASS
-- MQ_EXCHANGE
-- MQ_ROUTING_KEY
+- MQ_DATA_EXCHANGE (Exchange for record publishing)
+- MQ_DATA_ROUTING_KEY (Routing key for record publishing records)
+- MQ_PAGINATION_EXCHANGE (Exchange for solr offset publishing. Optional when running in consumer mode)
+- MQ_PAGINATION_ROUTING_KEY (Routing key for solr offset publishing. Optional when running in consumer mode)
+- MQ_PAGINATION_QUEUE (Queue name for pagination offsets)
 - MQ_QUEUE_DURABLE (optional, default value: True)
 - SOLR_COLLECTION_URL (ex: `http://solr.local:8983/solr/publication/select`)
 - SOLR_FETCH_SIZE (optional, default value: 20)
@@ -70,6 +72,17 @@ You can also call this library as standalone job command.  Just set required env
 - LAST_INDEX_DATE_FILE_PATH (File path for storing last indexed date. Ex: `/home/user/last_indexed_date.txt`)
 - WORKER_POOL_SIZE (optional, default value: 10)
 - LOG_LEVEL (Logging level. See: [Python logging module docs](https://docs.python.org/3/library/logging.html#logging-levels))
+- MODE (Mode selection for scaling. See. Scalability section.)
 
 **Example Kubernetes job:** 
  You can see it to [kube.yaml](kube.yaml)
+
+
+## Scalability
+
+This job can be scalable using multiple instances as of version 1.1.0. 
+If you are going to run a single instance, you don't need to set the MODE environment variable (or set it "DEFAULT"). 
+Otherwise, you need to set the MODE environment variable to PAGINATOR or DEFAULT for one instance and CONSUMER for others.
+
+PAGINATOR mode sends the offset values for the given query to MQ so that instances running in CONSUMER mode can work 
+independently on the same query. In DEFAULT mode, first PAGINATOR and then CONSUMER operations run.
--- solr2rabbitmq/__init__.py
+++ solr2rabbitmq/__init__.py
@@ -9,7 +9,6 @@ from jinja2 import Template
 
 
 async def run(loop, logger=None, config=None, worker_pool_size=10):
-
     async def _get_rabbitmq_channel():
         async with rabbitmq_connection_pool.acquire() as connection:
             return await connection.channel()
@@ -24,16 +23,15 @@ async def run(loop, logger=None, config=None, worker_pool_size=10):
             loop=loop
         )
 
-    async def _publish(message):
-        async with rabbitmq_channel_pool.acquire() as channel:
-            exchange = await channel.get_exchange(config.get("mq_exchange"))
-            await exchange.publish(
-                message=aio_pika.Message(message.encode("utf-8")),
-                routing_key=config.get("mq_routing_key")
-            )
+    async def _publish(message, channel, exchange, routing_key):
+        ex = await channel.get_exchange(exchange)
+        await ex.publish(
+            message=aio_pika.Message(message.encode("utf-8")),
+            routing_key=routing_key
+        )
 
-            if logger:
-                logger.debug(f"Document sent to queue: {message}")
+        if logger:
+            logger.debug(f"Document sent to queue: {message}")
 
     async def _save_last_index_date(query):
         with open(config.get("last_index_date_file_path"), "w+") as f:
@@ -45,45 +43,124 @@ async def run(loop, logger=None, config=None, worker_pool_size=10):
                     r = await resp.json()
                     f.write(r["response"]["docs"][0][config.get('solr_indexdate_field')])
 
-    async def _query_solr(worker_id, query, last_index_date):
-        nonlocal offset
-        template = Template(template_format, enable_async=True)
+    async def _query_solr_and_push(channel_pool, worker_id, query, last_index_date):
+        async with channel_pool.acquire() as channel:
+            queue = await channel.declare_queue(
+                config.get("mq_pagination_queue"), durable=config.get("mq_queue_durable"), auto_delete=False
+            )
+            template = Template(template_format, enable_async=True)
 
-        if logger:
-            logger.info(f"Worker-{worker_id} started.")
+            if logger:
+                logger.info(f"Worker-{worker_id} started.")
 
-        async with aiohttp.ClientSession() as session:
-            while True:
-                local_offset = offset
-                offset += config.get('solr_fetch_size')
-                url = f"{config.get('solr_collection_url')}"
+            async with aiohttp.ClientSession() as session:
+                while True:
+                    try:
+                        m = await queue.get(timeout=300)
+                        offset = m.body.decode('utf-8')
+                        url = f"{config.get('solr_collection_url')}"
 
-                if last_index_date:
-                    url += f"?fq={config.get('solr_indexdate_field')}:[{last_index_date} TO *]"
-                    url += f"&rows={config.get('solr_fetch_size')}"
-                else:
-                    url += f"?rows={config.get('solr_fetch_size')}"
-
-                url += f"&start={local_offset}"
-                url += f"&sort={config.get('solr_indexdate_field')} asc"
-
-                try:
-                    async with session.post(url=url, json=query) as resp:
-                        r = await resp.json()
-                        if len(r["response"]["docs"]) == 0:
-                            logger.debug("Worker closed, saving last index date...")
-                            await _save_last_index_date(query)
-                            break
+                        if last_index_date:
+                            url += f"?fq={config.get('solr_indexdate_field')}:[{last_index_date} TO *]"
+                            url += f"&rows={config.get('solr_fetch_size')}"
                         else:
-                            for doc in r["response"]["docs"]:
-                                try:
-                                    rendered_data = await template.render_async(doc=doc)
-                                except Exception as e:
-                                    logger.error(f"Error when rendering template: {e}. Solr document was: {doc}")
-                                await _publish(rendered_data)
-                except Exception as e:
-                    logger.error(f"Solr query or connection error")
-                    raise e
+                            url += f"?rows={config.get('solr_fetch_size')}"
+
+                        url += f"&start={offset}"
+                        url += f"&sort={config.get('solr_indexdate_field')} asc"
+
+                        try:
+                            async with session.post(url=url, json=query) as resp:
+                                r = await resp.json()
+                                if len(r["response"]["docs"]) == 0:
+                                    logger.debug("Worker closed, saving last index date...")
+                                    await _save_last_index_date(query)
+                                    break
+                                else:
+                                    for doc in r["response"]["docs"]:
+                                        try:
+                                            rendered_data = await template.render_async(doc=doc, json=json)
+                                        except Exception as e:
+                                            logger.error(
+                                                f"Error when rendering template: {e}. Solr document was: {doc}")
+                                        await _publish(rendered_data,
+                                                       channel,
+                                                       config.get("mq_data_exchange"),
+                                                       config.get("mq_data_routing_key"))
+
+                            m.ack()
+                        except Exception as e:
+                            logger.error(f"Solr query or connection error: {e}")
+
+                    except aio_pika.exceptions.QueueEmpty:
+                        if logger:
+                            logger.info("Consumer %s: Queue empty. Stopping." % worker_id)
+                        break
+
+    async def _publish_paginator(channel_pool, worker_id, fetch_size, step, left=None):
+        async with channel_pool.acquire() as channel:
+            start = worker_id * step * fetch_size
+            end = (worker_id + 1) * step * fetch_size
+
+            for i in range(start, end, fetch_size):
+                await _publish(str(i),
+                               channel,
+                               config.get("mq_pagination_exchange"),
+                               config.get("mq_pagination_routing_key"))
+            if left:
+                for i in range(end, end + left, fetch_size):
+                    await _publish(str(i),
+                                   channel,
+                                   config.get("mq_pagination_exchange"),
+                                   config.get("mq_pagination_routing_key"))
+
+    async def _run_consumer_mode(channel_pool):
+        for i in range(worker_pool_size):
+            try:
+                with open(config.get("solr_json_query_file_path")) as f:
+                    solr_query = f.read()
+            except Exception as e:
+                raise e
+            try:
+                with open(config.get("last_index_date_file_path")) as f:
+                    last_index_date = f.read()
+            except Exception as e:
+                last_index_date = None
+            worker_pool.append(
+                _query_solr_and_push(channel_pool=channel_pool,
+                                     worker_id=i,
+                                     query=json.loads(solr_query),
+                                     last_index_date=last_index_date)
+            )
+        await asyncio.gather(*worker_pool)
+
+    async def _run_paginator_mode(channel_pool):
+
+        record_number = 0
+
+        with open(config.get("solr_json_query_file_path")) as f:
+            solr_query = f.read()
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                    f"{config.get('solr_collection_url')}?rows=0",
+                    json=json.loads(solr_query)
+            ) as resp:
+                r = await resp.json()
+                record_number = r["response"]["numFound"]
+
+        fetch_size = config.get("solr_fetch_size")
+
+        bucket_size = fetch_size * worker_pool_size
+        round_step = record_number // bucket_size
+        round_left = record_number % bucket_size
+
+        for i in range(worker_pool_size):
+            if i == worker_pool_size - 1:
+                worker_pool.append(_publish_paginator(channel_pool, i, fetch_size, round_step, round_left))
+            else:
+                worker_pool.append(_publish_paginator(channel_pool, i, fetch_size, round_step))
+
+        await asyncio.gather(*worker_pool)
 
     rabbitmq_connection_pool = Pool(_get_rabbitmq_connection, max_size=worker_pool_size, loop=loop)
     rabbitmq_channel_pool = Pool(_get_rabbitmq_channel, max_size=worker_pool_size, loop=loop)
@@ -95,19 +172,22 @@ async def run(loop, logger=None, config=None, worker_pool_size=10):
             "mq_vhost": os.environ.get('MQ_VHOST'),
             "mq_user": os.environ.get('MQ_USER'),
             "mq_pass": os.environ.get('MQ_PASS'),
-            "mq_exchange": os.environ.get('MQ_EXCHANGE'),
-            "mq_routing_key": os.environ.get("MQ_ROUTING_KEY"),
+            "mq_data_exchange": os.environ.get('MQ_DATA_EXCHANGE'),
+            "mq_data_routing_key": os.environ.get("MQ_DATA_ROUTING_KEY"),
+            "mq_pagination_exchange": os.environ.get('MQ_PAGINATION_EXCHANGE'),
+            "mq_pagination_routing_key": os.environ.get("MQ_PAGINATION_ROUTING_KEY"),
+            "mq_pagination_queue": os.environ.get("MQ_PAGINATION_QUEUE"),
             "mq_queue_durable": bool(strtobool(os.environ.get('MQ_QUEUE_DURABLE', 'True'))),
             "solr_collection_url": os.environ.get("SOLR_COLLECTION_URL"),
-            "solr_fetch_size": int(os.environ.get("SOLR_FETCH_SIZE"), 20),
+            "solr_fetch_size": int(os.environ.get("SOLR_FETCH_SIZE")),
             "solr_indexdate_field": os.environ.get("SOLR_INDEXDATE_FIELD"),
             "solr_json_query_file_path": os.environ.get("SOLR_JSON_QUERY_FILE_PATH"),
             "data_template_file_path": os.environ.get("DATA_TEMPLATE_FILE_PATH"),
             "last_index_date_file_path": os.environ.get("LAST_INDEX_DATE_FILE_PATH"),
-            "worker_pool_size": os.environ.get("WORKER_POOL_SIZE")
+            "worker_pool_size": os.environ.get("WORKER_POOL_SIZE"),
+            "mode": os.environ.get("MODE")
         }
 
-    offset = 0
     template_format = open(config.get("data_template_file_path")).read()
 
     if "worker_pool_size" in config:
@@ -116,7 +196,7 @@ async def run(loop, logger=None, config=None, worker_pool_size=10):
                 worker_pool_size = int(config.get("worker_pool_size"))
             except TypeError as e:
                 if logger:
-                    logger.error(f"Invalid pool size: {config.get('worker_pool_size')}")
+                    logger.error("Invalid pool size: %s" % (worker_pool_size,))
                 raise e
 
     async with rabbitmq_connection_pool, rabbitmq_channel_pool:
@@ -124,22 +204,12 @@ async def run(loop, logger=None, config=None, worker_pool_size=10):
         if logger:
             logger.info("Workers started")
 
-        for i in range(worker_pool_size):
-            try:
-                with open(config.get("solr_json_query_file_path")) as f:
-                    solr_query = f.read()
-            except Exception as e:
-                raise e
-
-            try:
-                with open(config.get("last_index_date_file_path")) as f:
-                    last_index_date = f.read()
-            except Exception as e:
-                last_index_date = None
-
-            worker_pool.append(
-                _query_solr(worker_id=i, query=json.loads(solr_query), last_index_date=last_index_date)
-            )
-
-        await asyncio.gather(*worker_pool)
+        if config.get("mode") == "PAGINATOR":
+            await _run_paginator_mode(rabbitmq_channel_pool)
+        elif config.get("mode") == "CONSUMER":
+            await _run_consumer_mode(rabbitmq_channel_pool)
+        else:
+            await _run_paginator_mode(rabbitmq_channel_pool)
+            worker_pool.clear()
+            await _run_consumer_mode(rabbitmq_channel_pool)
 
